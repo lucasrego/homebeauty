7fe9dcbc094ca5187767a60483ce517c4bbfb5e6
Lucas Rêgo <lucasrego@gmail.com>
2014-03-11 21:09:12 -0300

    map, teste ajax

---
+++ homebeauty.php
@@ -0,0 +1,19 @@
+<?php
+
+// We don't need action for this tutorial, but in a complex code you need a way to determine Ajax action nature
+//$action = $_POST['action']; 
+// Decode JSON object into readable PHP object
+//$formData = json_decode($_POST['formData']); 
+
+// Get username
+//$username = $formData->{'username'}; 
+// Get password
+//$password = $formData->{'password'}; 
+
+header('Cache-Control: no-cache, must-revalidate');
+	
+$output = array('status' => true, 'massage' => 'Welcome!');
+echo json_encode($output);
+
+
+?>
