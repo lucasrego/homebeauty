d32aa22553b8e8132566196b03a26466247aa72e
lucasrego <lucasrego@gmail.com>
2014-03-12 14:59:24 -0300

    ajustes ajax dinamico dos servicos

--- homebeauty.php
+++ homebeauty.php
@@ -1,6 +1,5 @@
 <?php
 
-// We don't need action for this tutorial, but in a complex code you need a way to determine Ajax action nature
 //$action = $_POST['action']; 
 // Decode JSON object into readable PHP object
 //$formData = json_decode($_POST['formData']); 
@@ -12,7 +11,7 @@
 
 header('Cache-Control: no-cache, must-revalidate');
 
-echo '{ "resultado": "1", "servicos": "Escova|Cauterização" }';	
+echo '{ "resultado": "1", "servicos": "Escova|Cauterização" }';
 
 //$output = array('status' => true, 'massage' => 'Welcome!');
 //echo json_encode($output);
